eadda8c71674750dc914a07a3876524e95208005
sir-udenna <udennaboy@gmail.com>
2021-04-08 19:51:38 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time){
+  // console.log(time)
+  let t = parseInt(time,10)
+  if (t > 17){
+    return 'Good Evening'
+  }else if(t > 12 && t < 17){
+    return 'Good Afternoon'
+  }else{
+    return 'Good Morning'
+  }
+}
+
+function displayMessage(string){
+  let text = document.querySelector('#greeting')
+  text.innerText = string
+}
